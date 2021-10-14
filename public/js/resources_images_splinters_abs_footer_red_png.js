207bd80bcba575cde494c25fa3fabe6efbc22c87
Geiler Elias Radillo Sarmiento <37160395+geilerelias@users.noreply.github.com>
2021-10-13 20:04:23 -0500

    tercer commit

---
+++ public/js/resources_images_splinters_abs_footer_red_png.js
@@ -0,0 +1,18 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_images_splinters_abs_footer_red_png"],{
+
+/***/ "./resources/images/splinters/abs footer red.png":
+/*!*******************************************************!*\
+  !*** ./resources/images/splinters/abs-footer-red.png ***!
+  \*******************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/abs-footer-red.png?74c8e6000114864b437bda4451312718");
+
+/***/ })
+
+}]);
---
+++ routes/web.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiceController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/app-bar', function () {
+    return Inertia\Inertia::render('AppBar');
+});
+
+Route::get('/app-bar2', function () {
+    return Inertia\Inertia::render('AppBar2');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return Inertia\Inertia::render('Home');
+})->name('home');
+
+Route::get('/about-us', function () {
+    return Inertia\Inertia::render('AboutUs');
+})->name('about-us');
+
+Route::get('/our-services', function () {
+    return Inertia\Inertia::render('OurServices');
+})->name('our-services');
+
+Route::get('/gallery', function () {
+    return Inertia\Inertia::render('Gallery');
+})->name('gallery');
+
+Route::get('/contact-us', function () {
+    return Inertia\Inertia::render('ContactUs');
+})->name('contact-us');
+
+Route::get('/example', function () {
+    return Inertia\Inertia::render('Example');
+})->name('example');
+
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia\Inertia::render('Dashboard');
+})->name('dashboard');
+
+Route::get('/services/{id}', [ServiceController::class,'getImages']);
+
+Route::get('storage/{folder}/{filename}', function ($folder, $filename) {
+
+    try {
+        $path = storage_path() . '/app/' . $folder . '/' . $filename;
+
+        //si no se encuentra lanzamos un error 404.
+        if (!Storage::exists($folder . '/' . $filename)) {
+            abort(404);
+        }
+
+        $file = File::get($path);
+        $type = File::mimeType($path);
+
+        $response = Response::make($file, 200);
+        $response->header("Content-Type", $type);
+        return $response;
+    } catch (\Throwable $th) {
+        return $th->getMessage();
+    }
+});
